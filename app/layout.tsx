17fc0a72fdf639f5bcbb3699915ffa73de328736
RafaelTomazini <rafaelnt83@gmail.com>
2023-11-29 09:12:53 -0300

    initial commit: project strcture + nextJs

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Zini Finances",
+  description: "A easy finance management helper",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} bg-slate-800 mx-auto p-4 text-white`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
